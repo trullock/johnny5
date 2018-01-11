9ba3b75911093b96c7e4638d910cabf00228398c
Andrew Bullock <trullock@gmail.com>
2018-01-11 18:49:08 +0000

    test pwm code

--- johnny5.js
+++ johnny5.js
@@ -1,11 +1,14 @@
-var SAINT = require('./lib/saint.js');
+//var SAINT = require('./lib/saint.js');
 var Server = require('./lib/server.js');
 
-var j5 = new SAINT();
-var server = new Server(j5);
-		
-var j5Ready = j5.init();
+//var j5 = new SAINT();
+//var server = new Server(j5);
 
-j5Ready
-	.then(_ => j5.behaviour.idle())
-	.then(_ => server.start());
+var server = new Server(null);
+server.start();
+
+//var j5Ready = j5.init();
+
+//j5Ready
+//	.then(_ => j5.behaviour.idle())
+//	.then(_ => server.start());
